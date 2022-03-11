900fd1aa50e7d371e934464ad66ad203b5918f53
Tommaso Teofili <tteofili@redhat.com>
2022-03-11 15:27:49 +0100

    split data in train, test and valid, adjusted hyperparams

--- train_pytorch_tabular_fico.py
+++ train_pytorch_tabular_fico.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pytorch_tabular import TabularModel
 from pytorch_tabular.config import DataConfig, OptimizerConfig, TrainerConfig
 from pytorch_tabular.models import CategoryEmbeddingModelConfig
+from sklearn.model_selection import train_test_split
 
 data_config = DataConfig(
     target=['RiskPerformance'],
@@ -16,8 +17,8 @@ data_config = DataConfig(
 )
 trainer_config = TrainerConfig(
     auto_lr_find=True,  # Runs the LRFinder to automatically derive a learning rate
-    batch_size=1024,
-    max_epochs=100,
+    batch_size=512,
+    max_epochs=50,
     auto_select_gpus=False,
     gpus=0,  # index of the GPU to use. 0, means CPU
 )
@@ -25,9 +26,9 @@ optimizer_config = OptimizerConfig()
 
 model_config = CategoryEmbeddingModelConfig(
     task="classification",
-    layers="1024-512-512",  # Number of nodes in each layer
+    layers="100-500-100",  # Number of nodes in each layer
     activation="LeakyReLU",  # Activation between each layers
-    learning_rate=1e-3
+    learning_rate=1e-4
 )
 
 tabular_model = TabularModel(
@@ -36,9 +37,10 @@ tabular_model = TabularModel(
     optimizer_config=optimizer_config,
     trainer_config=trainer_config,
 )
-
-train_df = pd.read_csv('datasets/FICO/heloc_dataset_v1.csv')
-tabular_model.fit(train=train_df, validation=train_df[:10])  # TODO: use a validation set
-result = tabular_model.evaluate(train_df[-10:])  # TODO: use a test set
+data_df = pd.read_csv('datasets/FICO/heloc_dataset_v1.csv')
+train_df, test_df = train_test_split(data_df, test_size=0.2)
+train_df, valid_df = train_test_split(train_df, test_size=0.1)
+tabular_model.fit(train=train_df, validation=valid_df)
+result = tabular_model.evaluate(test_df)
 tabular_model.save_model("models/fico_tabular_basic")
 print(result)
